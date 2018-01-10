db59adc9e0e5fb6daf75e65e2f2f5242874cc26e
CryingUglyDuckling <naushabazerin113@gmail.com>
2018-01-10 14:00:52 +0600

    2nd commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from "./hero";
+
+export const HEROES:Hero[]=[
+    {id:11,name:"Jagornaut"},{id:16,name:"JagornautE"},
+    {id:12,name:"JagornautA"},{id:17,name:"JagornautF"},
+    {id:13,name:"JagornautB"},{id:18,name:"JagornautG"},
+    {id:15,name:"JagornautC"},{id:19,name:"JagornautH"},
+    {id:14,name:"JagorzdcsnautD"},{id:20,name:"JagornautI"}
+];
